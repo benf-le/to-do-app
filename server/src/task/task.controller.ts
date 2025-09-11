fd2ce9f9e40b8ec18af6e6d8dcb606761abddf7e
benf-le <banglebang2016@gmail.com>
2025-09-12 00:22:21 +0700

    feat(task): add get task

--- server/src/task/task.controller.ts
+++ server/src/task/task.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
 
+  @Get('/')
+  async getTask() {
+    return await this.taskService.getAllTasks();
+  }
 }
--- server/src/task/task.service.ts
+++ server/src/task/task.service.ts
@@ -5,4 +5,11 @@ import { PrismaService } from '../prisma/prisma.service';
 export class TaskService {
   constructor(private prismaService: PrismaService) {}
 
+  async getAllTasks() {
+    try {
+      return await this.prismaService.task.findMany();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
