976745fa6ffb4f7e1a33e01ff62595e304ce1ca0
benf-le <banglebang2016@gmail.com>
2025-09-12 01:16:00 +0700

    refactor: use GlobalExceptionFilter to catch error

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Gắn filter toàn cục
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
