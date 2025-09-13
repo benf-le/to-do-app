09a800ad5236e6ec7002c8bd2c8c98c6bb94c80a
benf-le <banglebang2016@gmail.com>
2025-09-14 04:39:11 +0700

    feat: add route TodayListView

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,8 +1,13 @@
 import TaskListPage from "../pages/TaskListPage.tsx";
+import TodayTaskListView from "../pages/TodayTaskListView.tsx";
 
 export const routes=[
     {
         path:'/',
         page: TaskListPage
+    },
+    {
+        path:'/today',
+        page: TodayTaskListView
     }
 ]
