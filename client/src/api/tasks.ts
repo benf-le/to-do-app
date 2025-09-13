3c989bfdd4df5f34523030e32510efb99d1d539b
benf-le <banglebang2016@gmail.com>
2025-09-13 23:02:06 +0700

    feat: add endpoint update task change endpoint

--- client/src/api/tasks.ts
+++ client/src/api/tasks.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import type { Task } from "../models/task";
+
 const API_URL = "http://localhost:3000"; // NestJS backend
 
 export const getTasks = async () => {
@@ -7,11 +9,16 @@ export const getTasks = async () => {
 };
 
 export const createTask = async (task: any) => {
-    const res = await axios.post(`${API_URL}/tasks`, task);
+    const res = await axios.post(`${API_URL}/tasks/create`, task);
+    return res.data;
+};
+
+export const updateTask = async (id: string, task: Partial<Task>) => {
+    const res = await axios.patch(`${API_URL}/tasks/update/${id}`, task);
     return res.data;
 };
 
 export const deleteTask = async (id: string) => {
-    const res = await axios.delete(`${API_URL}/tasks/${id}`);
+    const res = await axios.delete(`${API_URL}/tasks/delete/${id}`);
     return res.data;
 };
