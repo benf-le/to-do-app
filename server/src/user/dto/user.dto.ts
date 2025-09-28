e0261f00a2f49a4ec25dd7200bdddae04eee836e
benf-le <banglebang2016@gmail.com>
2025-09-29 02:16:03 +0700

    feat: add module user

---
+++ server/src/user/dto/user.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEmail,
+  IsString,
+  IsNotEmpty,
+  IsEnum,
+  IsBoolean,
+} from 'class-validator';
+import { UserType } from '@prisma/client';
+
+export class UserDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  @IsEnum(UserType)
+  userType: UserType;
+}
