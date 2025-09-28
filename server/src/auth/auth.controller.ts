7d05487ce8495f6b2c37f84d9c9f16e1c6ea8521
benf-le <banglebang2016@gmail.com>
2025-09-29 02:12:26 +0700

    feat: add login and register user

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import {Body, Controller, Param, ParseEnumPipe, Post, UnauthorizedException} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDTO, RegisterDTO } from './dto/auth.dto';
+import { UserType } from '@prisma/client';
+
+@Controller({})
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/register/:userType')
+  async register(@Body() registerDTO: RegisterDTO, @Param('userType', new ParseEnumPipe(UserType)) userType:UserType) {
+    if (userType!==UserType.USER){
+      return "This feature is in development";
+    }
+    return await this.authService.register(registerDTO, userType);
+  }
+
+  @Post('/login')
+  async login(@Body() loginDTO: LoginDTO) {
+    return await this.authService.login(loginDTO);
+  }
+}
