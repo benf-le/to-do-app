d6a5719538e32cc67a19245c4e6a31f6c3eaf754
benf-le <banglebang2016@gmail.com>
2025-09-29 02:19:09 +0700

    feat: add auth module into app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TaskModule } from './task/task.module';
     }),
     PrismaModule,
     TaskModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
