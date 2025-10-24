1ddfa07ec95214efa954bcb879320ccdf80fa7d6
benf-le <banglebang2016@gmail.com>
2025-10-24 21:46:07 +0700

    change cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,13 +4,13 @@ import { GlobalExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://todoapp.lecambang.id.vn',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  // Gắn filter toàn cục
-  app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: ['http://localhost:5173'],
+      credentials: true,
+    });
+  }
+  // app.setGlobalPrefix('api'); // nếu bạn dùng prefix /api ở Nest, nhớ điều chỉnh proxy ở Nginx tương ứng
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
