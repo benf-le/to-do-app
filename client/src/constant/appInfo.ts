5e79020af34c37fabcbe956c1a4e9e074e8d3326
Lê Cẩm Bằng <102325425+benf-le@users.noreply.github.com>
2025-10-24 22:24:04 +0700

    Update BASE_URL

--- client/src/constant/appInfo.ts
+++ client/src/constant/appInfo.ts
@@ -1,4 +1,4 @@
-export const BASE_URL =`http://localhost:3000`
-// export const BASE_URL =`https://be-verifysupp.lecambang.id.vn`
+//export const BASE_URL =`http://localhost:3000`
+export const BASE_URL =`https://be-verifysupp.lecambang.id.vn`
 // export const CHATWOOT_URL=`http://34.55.138.114:3000` run local
 export const CHATWOOT_URL=`https://chatwoot.lecambang.id.vn` //run gcp
