e67cd658a988f3a3b9b4d9e69710604e572a3c6b
benf-le <banglebang2016@gmail.com>
2025-10-26 22:09:10 +0700

    fix Cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,17 +4,11 @@ import { GlobalExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (process.env.NODE_ENV !== 'production') {
-    app.enableCors({
-      origin: ['http://localhost:5173'],
-      credentials: true,
-    });
-  } else {
-    app.enableCors({
-      origin: ['http://todolist.io.vn'],
-      credentials: true,
-    });
-  }
+  app.enableCors({
+    origin: 'https://todoapp.lecambang.id.vn',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // app.setGlobalPrefix('api'); // nếu bạn dùng prefix /api ở Nest, nhớ điều chỉnh proxy ở Nginx tương ứng
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
