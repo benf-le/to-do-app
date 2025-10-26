261f7bc19c4e12e135b293841ec3a456530e057b
benf-le <banglebang2016@gmail.com>
2025-10-26 22:12:08 +0700

    fix Cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://todoapp.lecambang.id.vn',
+    origin: 'https://todolist.io.vn',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
