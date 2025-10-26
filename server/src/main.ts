1ac8a851c6fec69358d398157a86816d138a58ea
benf-le <banglebang2016@gmail.com>
2025-10-26 22:05:26 +0700

    fix Cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,9 +8,14 @@ async function bootstrap() {
     app.enableCors({
       origin: ['http://localhost:5173'],
       credentials: true,
-
+    });
+  } else {
+    app.enableCors({
+      origin: ['http://todolist.io.vn'],
+      credentials: true,
     });
   }
+
   // app.setGlobalPrefix('api'); // nếu bạn dùng prefix /api ở Nest, nhớ điều chỉnh proxy ở Nginx tương ứng
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
