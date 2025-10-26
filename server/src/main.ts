67e917189d5f997d5ae1866060ce0d5cbb18555f
benf-le <banglebang2016@gmail.com>
2025-10-26 22:09:20 +0700

    fix Cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
